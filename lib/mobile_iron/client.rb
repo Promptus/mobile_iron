fa9dcf25cd07c8a3a117c19b66378d04718281a5
Lars Kuhnt <lars.kuhnt@gmail.com>
2016-12-07 17:43:44 +0100

    added app data update call

--- lib/mobile_iron/client.rb
+++ lib/mobile_iron/client.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 require 'faraday'
 require 'oj'
+require 'logger'
 
 module MobileIron
   class Client
@@ -14,16 +15,18 @@ module MobileIron
 
     def upload_inhouse_app(file_path:)
       with_connection do |connection|
-        upload_io = Faraday::UploadIO.new(file_path, 'application/octet-stream')
-        response = connection.post '/api/v2/appstore/inhouse', params('installer': upload_io)
+        params = { 'installer': Faraday::UploadIO.new(file_path, 'application/octet-stream') }
+        response = connection.post '/api/v2/appstore/inhouse', params.merge(admin_device_space_param)
         raise MobileIron::RequestError, "Failed to upload app '#{file_path}': #{response.status} #{response.body}" if response.status != 200
         Oj.load(response.body)
       end
     end
 
-    def update_app_data(id:, data: {})
+    def update_app_data(id:, data: {}, icon_file_path: nil)
       with_connection do |connection|
-        response = connection.post "/api/v2/appstore/apps/#{id}", params(data)
+        params = { 'appData': data.to_json }
+        params['icon'] = Faraday::UploadIO.new(icon_file_path, 'image/png') if icon_file_path
+        response = connection.post "/api/v2/appstore/apps/#{id}", params.merge(admin_device_space_param), { 'Content-Type': 'multipart/form-data' }
         raise MobileIron::RequestError, "Failed to update data of app '#{id}': #{response.status} #{response.body}" if response.status != 200
         Oj.load(response.body)
       end
@@ -34,15 +37,15 @@ module MobileIron
     def with_connection
       connection = Faraday.new(url: @url) do |f|
         f.request :multipart
-        f.request :url_encoded
-        f.adapter Faraday.default_adapter
+        # f.response :logger, ::Logger.new(STDOUT), bodies: true
         f.basic_auth @username, @password
+        f.adapter :net_http
       end
       yield connection
     end
 
-    def params(extra_params)
-      { 'adminDeviceSpaceId': @admin_device_space_id }.merge(extra_params)
+    def admin_device_space_param
+      { 'adminDeviceSpaceId': @admin_device_space_id }
     end
 
   end
